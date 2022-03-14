41e81d811acdad805d3f65f84659b65e7c6d3827
Anson Cheung <anson@Ansons-MacBook-Pro.local>
2022-03-14 22:09:00 +0000

    feat: add inputText

---
+++ src/components/Input/InputText.js
@@ -0,0 +1,61 @@
+import styles from './InputText.module.scss'
+import { useState } from 'react';
+
+export const InputText = ({
+    required=true,
+    label='1. Lorem ipsum',
+    placeHolder='type your text here...',
+    instructionType='toggle', //toggle or link
+    instruction={
+        link: '#',
+        linkTargetBlank: false,
+        linkText: 'Instructions',
+        toggleTextShow: 'show instructions',
+        toggleTextHide: 'hide instructions',
+        toggleContent: 'your content here...'
+    },
+    onChange,
+}) => {
+
+    const [toggle, setToggle] = useState(false);
+
+    const toggleText = (e) => {
+        e.target.innerText = toggle ? instruction.toggleTextShow : instruction.toggleTextHide;
+
+        setToggle( !toggle );
+    }
+
+    return (
+        <>
+            {/* Label */}
+            <div className={styles.label}>
+                { label }
+                { required ? <span>*</span> : ''}
+                {
+                    instructionType == 'link' ? 
+                    <a href={instruction.link} target={instruction.linkTargetBlank ? '_blank' : ''}>{instruction.linkText}</a> : 
+                    ''
+                }
+                {
+                    instructionType == 'toggle' ? 
+                    <>
+                        <a onClick={(e) => toggleText(e)}>{ instruction.toggleTextShow }</a>
+                        {
+                            toggle ? 
+                            <div>
+                                { instruction.toggleContent }
+                            </div> :
+                            ''
+                        }
+                    </> : 
+                    ''
+                }
+
+            </div>
+            {/* Input */}
+            <div className={styles.input}>
+                <input onChange={(e) => onChange(e)} placeholder={placeHolder} type="text" />
+            </div>
+        </>
+    );
+}
---
+++ src/components/Input/InputText.stories.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { InputText } from './InputText';
+
+export default {
+    title: 'Input/Text',
+    component: InputText,
+
+
+}
+
+const Template = (args) => <InputText {...args} />;
+
+export const Toggle = Template.bind({});
+Toggle.args = {
+    required: true,
+    label:'1. Lorem ipsum',
+    placeHolder:'type your text here...',
+    instructionType:'toggle', //toggle or link
+    instruction:{
+        toggleTextShow: 'show instructions',
+        toggleTextHide: 'hide instructions',
+        toggleContent: 'your content here...'
+    },
+    onChange: (e) => console.log(e.target.value),
+}
+
+export const Link = Template.bind({});
+Link.args = {
+    required: true,
+    label: '1. Lorem ipsum',
+    placeHolder: 'type your text here...',
+    instructionType: 'link', //toggle or link
+    instruction:{
+        link: '#',
+        linkTargetBlank: false,
+        linkText: 'Instructions',
+    },
+    onChange: (e) => console.log(e.target.value),
+}
